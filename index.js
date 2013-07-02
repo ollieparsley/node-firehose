48688557494ae35dc2175f508b1a90c49ed434e6
ollie <ollie@ollieparsley.com>
2013-07-02 08:52:24 +0100

    Added router into main entry

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
 	Authentication: require("./lib/authentication/base").Authentication,
+	Router:         require("./lib/router/base").Router,
 	Store:          require("./lib/store/base").Store,
 	Transport:      require("./lib/authentication/base").Transport,
 	User:           require("./lib/user/base").User,
