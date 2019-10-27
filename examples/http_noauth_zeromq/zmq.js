2762c880233062ee98cc8fe2a9df1dd4ca5590bf
Ollie Parsley <ollie@ollieparsley.com>
2019-10-27 22:01:53 +0000

    Brought libraries up-to-date and changed websocket library

--- examples/http_noauth_zeromq/zmq.js
+++ examples/http_noauth_zeromq/zmq.js
@@ -1,5 +1,5 @@
 //Test script that will send a ZMQ message a few times a second
-var zmq = require("zmq");
+var zmq = require("zeromq");
 var socket = zmq.createSocket("pub");
 var endpoint = "tcp://127.0.0.1:1234";
 socket.bind(endpoint, function(){
